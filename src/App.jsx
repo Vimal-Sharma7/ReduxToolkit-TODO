5a02c09213c779e3e6447a28a91957cbe90994bf
vimalsharma <vimalsharma10082000@gmail.com>
2024-05-27 13:30:56 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+
+import './App.css'
+import Todo from './components/Todo'
+import AddTodo from './components/AddTodo'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1>React Redux ToolKit</h1>
+      <AddTodo />
+      <Todo />
+    </>
+  )
+}
+
+export default App
